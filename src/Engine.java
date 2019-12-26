1c7d84a59b5bdaccbab4e666a0f2a92ab7bea745
Simon <smchase@protonmail.com>
2019-12-26 13:34:42 -0500

    better

--- src/Engine.java
+++ src/Engine.java
@@ -1,39 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-import java.awt.Graphics;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Engine extends JPanel
+public class Engine
 {
-    int x = 200, y = 0;
-    int velX = 0, velY = 1;
-
-    private void move()
+    public static void main(String[] args)
     {
-        x += velX;
-        y += velY;
+        SwingUtilities.invokeLater(Engine::createAndShowGUI);
     }
 
-    @Override
-    public void paint(Graphics g)
+    private static void createAndShowGUI()
     {
-        super.paint(g);
-        g.fillOval(x, y, 200, 200);
-    }
-
-    public static void main(String[] args) throws InterruptedException
-    {
-        JFrame jf = new JFrame("Physics Engine");
-        Engine eng = new Engine();
-        jf.add(eng);
-        jf.setSize(400, 400);
-        jf.setVisible(true);
+        System.out.println("Created GUI on EDT? "+SwingUtilities.isEventDispatchThread());
+        JFrame jf = new JFrame("Pool Engine");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.add(new MyPanel());
+        jf.pack();
+        jf.setVisible(true);
+    }
+}
 
-        for (int i = 0; i < 1000; i++) {
-            eng.move();
-            eng.repaint();
-            Thread.sleep(100);
+class MyPanel extends JPanel
+{
+    private int squareX = 50;
+    private int squareY = 50;
+    private int squareW = 20;
+    private int squareH = 20;
+
+    public MyPanel()
+    {
+
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+
+    }
+
+    private void moveSquare(int x, int y) {
+        int OFFSET = 1;
+        if ((squareX!=x) || (squareY!=y)) {
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+            squareX=x;
+            squareY=y;
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
         }
+    }
+
+
+    public Dimension getPreferredSize() {
+        return new Dimension(250,200);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawString("This is my custom Panel!",10,20);
+        g.setColor(Color.RED);
+        g.fillRect(squareX,squareY,squareW,squareH);
+        g.setColor(Color.BLACK);
+        g.drawRect(squareX,squareY,squareW,squareH);
     }
 }
