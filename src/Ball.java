bfad902bef4603681a580e12bd3f4906222503f5
alexboden <43760105+alexboden@users.noreply.github.com>
2020-01-08 14:05:44 -0500

    d

--- src/Ball.java
+++ src/Ball.java
@@ -2,7 +2,7 @@ import java.awt.*;
 
 class Ball
 {
-    private double xPos, yPos, diam, vel, dir;
+    private double xPos, yPos, diam, xVel,  yVel;
     private Color col;
 
     public Ball(int x, int y, int d, int[] rgb)
@@ -10,15 +10,15 @@ class Ball
         xPos = x;
         yPos = y;
         diam = d;
-        vel = 50;
-        dir = 0;
+        xVel = 0;
+        yVel = 0;
         col = new Color(rgb[0], rgb[1], rgb[2]);
     }
 
     public void applyTime(long t)
     {
-        xPos += vel*Math.cos(dir)*t/Math.pow(10, 9);
-        yPos += vel*Math.sin(dir)*t/Math.pow(10, 9);
+        xPos += xVel*t/Math.pow(10, 9);
+        yPos += yVel*t/Math.pow(10, 9);
     }
 
     public void displayBall(Graphics g)
