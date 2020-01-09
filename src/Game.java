da4df6e24cd5f1293c77fa50bf85ede53048f2f4
alexboden <43760105+alexboden@users.noreply.github.com>
2020-01-09 15:05:15 -0500

    side wall done

--- src/Game.java
+++ src/Game.java
@@ -8,30 +8,51 @@ public class Game extends JPanel
     private final int RADIUS = 20, FPS = 60, WIDTH = 600, HEIGHT = 400;
     private final long START_TIME = System.nanoTime(), TIME_INCREMENT = (long)Math.pow(10, 9)/FPS;
     private Ball[] balls = {
-            new Ball(150, 200, RADIUS, new int[]{255, 0, 0}),
-            new Ball(450, 200, RADIUS, new int[]{255, 0, 0})
+            new Ball(200, 200, RADIUS, new int[]{255, 0, 0}),
+            new Ball(400, 200, RADIUS, new int[]{255, 0, 0})
     };
     private PriorityQueue<Collision> collisions = new PriorityQueue<>();
 
     public Game()
     {
         balls[0].applyVel(50, 50);
+
         for (int i = 0; i < balls.length; i++) {
             if (balls[i].getyVel() > 0) {
-                System.out.println("vel over 0");
                 collisions.add(new WallCollision(
-                        (long)((HEIGHT - RADIUS - balls[i].getyPos()) / balls[i].getyVel() * Math.pow(10, 9)),
+                        (long) ((HEIGHT - RADIUS - balls[i].getyPos()) / balls[i].getyVel() * Math.pow(10, 9)),
                         i,
                         false
                 ));
             } else if (balls[i].getyVel() < 0) {
                 collisions.add(new WallCollision(
-                        (long)((RADIUS - balls[i].getyPos()) / balls[i].getyVel() * Math.pow(10, 9)),
+                        (long) ((RADIUS - balls[i].getyPos()) / balls[i].getyVel() * Math.pow(10, 9)),
                         i,
                         false
                 ));
             }
+
+            if (balls[i].getxVel() > 0) {
+                collisions.add(new WallCollision(
+                        (long) ((WIDTH - RADIUS - balls[i].getxPos()) / balls[i].getxVel() * Math.pow(10, 9)),
+                        i,
+                        true
+                ));
+            } else if (balls[i].getxVel() < 0) {
+                collisions.add(new WallCollision(
+                        (long) ((RADIUS - balls[i].getxPos()) / balls[i].getxVel() * Math.pow(10, 9)),
+                        i,
+                        true
+                ));
+            }
         }
+
+        for (int i = 0; i < balls.length-1; i++) {
+            for (int j = 0; j < balls.length; j++) {
+
+            }
+        }
+
 
         while (true)
         {
